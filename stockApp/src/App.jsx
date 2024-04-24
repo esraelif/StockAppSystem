162b342d37743a3e4e6104db1c18c33d3ba2404b
esraelif <elifesratunca@gmail.com>
2024-04-24 11:45:36 +0200

    reduxPersist

--- stockApp/src/App.jsx
+++ stockApp/src/App.jsx
@@ -1,8 +1,8 @@
 import { ThemeProvider, createTheme } from "@mui/material"
 import AppRouter from "./router/AppRouter"
 import { Provider } from "react-redux";
-import store from "./app/store";
-
+import store, { persistor } from "./app/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 
@@ -25,7 +25,9 @@ function App() {
 
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <AppRouter />
+          <PersistGate loading={null} persistor={persistor}>
+            <AppRouter />
+          </PersistGate>
         </Provider>
 
       </ThemeProvider>
--- stockApp/src/app/store.js
+++ stockApp/src/app/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
 
 const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistedReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
 });
+export let persistor = persistStore(store);
 export default store;
