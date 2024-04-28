592f9964f9b338e7ae1388dc8e3314444e3414d9
esraelif <elifesratunca@gmail.com>
2024-04-28 19:12:49 +0200

    KpiCards

--- stockApp/src/pages/Home.jsx
+++ stockApp/src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import Container from "@mui/material/Container";
+import React from "react";
+import PageHeader from "../components/Commons/PageHeader";
+import Charts from "../components/Dashboard/Charts";
+import KpiCards from "../components/Dashboard/KpiCards";
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <Container maxWidth={"xl"}>
+            <PageHeader text="Dashboard" />
+            <KpiCards />
+            <Charts />
+        </Container>
     );
-}
+};
 
 export default Home;
