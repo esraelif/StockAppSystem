e7ef9b5c4562f09463dd87809dc88ebe452ca32d
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 21:21:14 +0200

    Update authSlice.jsx

--- stockApp/src/features/authSlice.jsx
+++ stockApp/src/features/authSlice.jsx
@@ -1,46 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-        currentUser: null,
-        loading: false,
-        error: false,
-        token: null,
-    }, reducers: {
-        fetchStart: state => {
-            state.loading = true;
-            state.error = false;
-        },
-        registerSuccess: (state, { payload }) => {
-            state.loading = false;
-            state.currentUser = payload.data.username;
-            state.token = payload.token
-        },
-        //! User bilgisi farklı geldiği için ayrı slice açtık.
-        loginSuccess: (state, { payload }) => {
-            state.loading = false;
-            state.currentUser = payload?.user?.username;
+  name: "auth",
 
-            state.token = payload?.token;
-        },
-        logoutSuccess: (state) => {
-            state.loading = false;
-            state.currentUser = null;
-            state.token = null;
-        },
-        fetchFail: state => {
-            state.loading = false;
-            state.error = true;
-        }
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: state => {
+      state.loading = true;
+      state.error = false;
     },
-})
+    registerSuccess: (state,{payload})=>{
+      state.loading = false;
+      state.currentUser = payload.data.username
+      state.token = payload.token
+    },
+     //! user bilgisi farklı geldiği için ayrı slice açtık
+     loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload?.user?.username;
+      state.token = payload?.token;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.currentUser = null;
+      state.token = null;
+    },
+    fetchFail: state => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
 export const {
-    fetchStart,
-    registerSuccess,
-    logoutSuccess,
-    loginSuccess,
-    fetchFail,
+  fetchStart,
+  registerSuccess,
+  logoutSuccess,
+  loginSuccess,
+  fetchFail,
 } = authSlice.actions;
 export default authSlice.reducer;
-
