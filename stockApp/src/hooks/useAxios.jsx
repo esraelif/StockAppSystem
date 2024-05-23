5c1e3bbe4adf01cbcc2231d15c4602f426ef6c1f
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 21:39:59 +0200

    Update useAxios.jsx

--- stockApp/src/hooks/useAxios.jsx
+++ stockApp/src/hooks/useAxios.jsx
@@ -2,16 +2,16 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 export const axiosWithPublic = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 const useAxios = () => {
-    const { token } = useSelector((state) => state.auth);
-    const axiosWithToken = axios.create({
-        baseURL: import.meta.env.VITE_BASE_URL,
-        headers: { Authorization: `Token ${token}` },
-    });
-    return axiosWithToken;
+  const { token } = useSelector((state) => state.auth);
+  const axiosWithToken = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: { Authorization: `Token ${token}` },
+  });
+  return axiosWithToken;
 };
 
 export default useAxios;
