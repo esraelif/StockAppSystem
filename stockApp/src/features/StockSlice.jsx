af0af1ab0f18e3e31824780b8559af9321161517
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 21:22:00 +0200

    Update StockSlice.jsx

--- stockApp/src/features/StockSlice.jsx
+++ stockApp/src/features/StockSlice.jsx
@@ -1,76 +1,82 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const stockSlice = createSlice({
-    name: "stock",
+  name: "stock",
 
-    initialState: {
-        categories: [],
-        brands: [],
-        firms: [],
-        products: [],
-        purchases: [],
-        sales: [],
-        loading: false,
-        error: false,
+  initialState: {
+    categories: [],
+    brands: [],
+    firms: [],
+    products: [],
+    purchases: [],
+    sales: [],
+    loading: false,
+    error: false,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
     },
-    reducers: {
-        fetchStart: (state) => {
-            state.loading = true;
-            state.error = false;
-        },
-        // firmsSuccess: (state, { payload }) => {
-        //   state.loading = false;
-        //   state.firms = payload;
-        // },
-        // brandsSuccess: (state, { payload }) => {
-        //   state.loading = false;
-        //   state.brands = payload;
-        // },
-        // getSuccess: (state, { payload }) => {
-        //   console.log(payload);
-        //   state.loading = false;
-        //   // state.brands = payload;
-        //   state[payload.url] = payload.data
-        // },
-        getSuccess: (state, { payload: { data, url } }) => {
-            // console.log(payload);
-            state.loading = false;
-            state[url] = data; // state["brands"] - state["firms"]
-        },
-        getProCatBrandSuccess: (state, { payload }) => {
-            state.loading = false;
-            state.products = payload[0];
-            state.categories = payload[1];
-            state.brands = payload[2];
-        },
-        getProPurcFirBrandsSuccess: (state, { payload }) => {
-            state.loading = false;
-            state.products = payload[0].data;
-            state.purchases = payload[1].data;
-            state.firms = payload[2].data;
-            state.brands = payload[3].data;
-        },
-        getProSalBrandsSuccess: (state, { payload }) => {
-            state.loading = false;
-            state.products = payload[0].data;
-            state.brands = payload[1].data;
-            state.sales = payload[2].data;
-        },
-        fetchFail: (state) => {
-            state.loading = false;
-            state.error = true;
-        },
+    // firmsSuccess: (state, { payload }) => {
+    //   state.loading = false;
+    //   state.firms = payload;
+    // },
+    // brandsSuccess: (state, { payload }) => {
+    //   state.loading = false;
+    //   state.brands = payload;
+    // },
+    // getSuccess: (state, { payload }) => {
+    //   console.log(payload);
+    //   state.loading = false;
+    //   // state.brands = payload;
+    //   state[payload.url] = payload.data
+    // },
+    getSuccess: (state, { payload:{data,url} }) => {
+      // console.log(payload);
+      state.loading = false;
+      state[url] = data; // state["brands"] - state["firms"]
     },
+    getProCatBrandSuccess : (state, { payload }) => {
+      state.loading = false;
+      state.products = payload[0];
+      state.categories = payload[1];
+      state.brands = payload[2];
+    },
+    getProPurcFirBrandsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.products = payload[0].data;
+      state.purchases = payload[1].data;
+      state.firms = payload[2].data;
+      state.brands = payload[3].data;
+    },
+    getProSalBrandsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.products = payload[0].data;
+      state.brands = payload[1].data;
+      state.sales = payload[2].data;
+    },
+    getPurcSalesSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.purchases= payload[1].data;
+      state.sales = payload[0].data;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 });
 
 export const {
-    fetchStart,
-    // firmsSuccess,
-    getSuccess,
-    // brandsSuccess,
-    getProCatBrandSuccess,
-    getProPurcFirBrandsSuccess,
-    getProSalBrandsSuccess,
-    fetchFail,
+  fetchStart,
+  // firmsSuccess,
+  getSuccess,
+  // brandsSuccess,
+  getProCatBrandSuccess,
+  getProPurcFirBrandsSuccess,
+  getProSalBrandsSuccess,
+  getPurcSalesSuccess,
+  fetchFail,
 } = stockSlice.actions;
 export default stockSlice.reducer;
