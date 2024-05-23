ca50d2682061a2fad3ed0623424c4e2d98127e17
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:44:54 +0200

    Delete stockApp/src/components/Commons/Loading.jsx

--- stockApp/src/components/Commons/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loading = () => {
-  return (
-    <div className='flex justify-center h-[70vh] items-center'><span className="loader"></span></div>
-  )
-}
-
-export default Loading
