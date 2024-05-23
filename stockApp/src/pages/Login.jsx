a5241be8ac538f7366de080f8852b3631e3aeacd
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:33:09 +0200

    Update Login.jsx

--- stockApp/src/pages/Login.jsx
+++ stockApp/src/pages/Login.jsx
@@ -1,71 +1,66 @@
+import LockIcon from "@mui/icons-material/Lock";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import { useTheme } from "@mui/material";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import LockIcon from "@mui/icons-material/Lock";
-import { Link } from "react-router-dom";
-import AuthHeader from "../components/AuthHeader";
-import AuthImage from "../components/AuthImage";
 import { Formik } from "formik";
-import LoginForm, { loginScheme } from "../components/LoginForm";
+import { Link } from "react-router-dom";
+import image from "../assets/hero.png";
+import AuthHeader from "../components/Commons/AuthHeader";
+import AuthImage from "../components/Commons/AuthImage";
+import LoginForm, { loginScheme } from "../components/Forms/LoginForm";
 import useAuthCall from "../hooks/useAuthCall";
 
 const Login = () => {
-    const theme = useTheme();
-    const { login } = useAuthCall();
+  const { login } = useAuthCall();
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <AuthHeader />
 
-    return (
-        <Container maxWidth="lg">
-            <Grid
-                container
-                justifyContent="center"
-                direction="row-reverse"
-                sx={{
-                    height: "100vh",
-                    p: 2,
-                }}
-            >
-                <AuthHeader />
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.main",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography variant="h4" align="center" mb={4} color="secondary.main">
+            SIGN IN
+          </Typography>
 
-                <Grid item xs={12} sm={10} md={6}>
-                    <Avatar
-                        sx={{
-                            backgroundColor: "secondary.main",
-                            m: "auto",
-                            width: 40,
-                            height: 40,
-                        }}
-                    >
-                        <LockIcon size="30" />
-                    </Avatar>
-                    <Typography variant="h4" align="center" mb={4} color="secondary.main">
-                        SIGN IN
-                    </Typography>
-                    <Formik
-                        initialValues={{ email: "", password: "" }}
-                        validationSchema={loginScheme}
-                        onSubmit={(values, actions) => {
-                            login(values);
-                            actions.resetForm();
-                            actions.setSubmitting(false);
-                        }}
-                        component={props => <LoginForm {...props} />}>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginScheme}
+            onSubmit={(values, actions) => {
+              login(values);
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+            component={(props) => <LoginForm {...props} />}
+          ></Formik>
+          <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
+            <Link to="/register">Don't have an account? Sign Up</Link>
+          </Box>
+        </Grid>
 
-                    </Formik>
-
-                    <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
-                        <Link to="/register">
-                            Don't have an account? Sign Up
-                        </Link>
-                    </Box>
-                </Grid>
-
-                <AuthImage />
-            </Grid>
-        </Container>
-    );
+        <AuthImage image={image} />
+      </Grid>
+    </Container>
+  );
 };
 
 export default Login;
