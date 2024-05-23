5f279a3ea37e0781b2fbb80ddc1782556b5b8e8a
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:38:08 +0200

    Update store.js

--- stockApp/src/app/store.js
+++ stockApp/src/app/store.js
@@ -1,39 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
-    FLUSH,
-    PAUSE,
-    PERSIST,
-    persistReducer,
-    persistStore,
-    PURGE,
-    REGISTER,
-    REHYDRATE,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "../features/authSlice";
-import { stocksApi } from "../services/stocks";
+import stockReducer from "../features/stockSlice";
 
 const persistConfig = {
-    key: "root",
-    storage,
+  key: "root",
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-    reducer: {
-        auth: persistedReducer,
-        [stocksApi.reducerPath]: stocksApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => {
-        const defaultMiddleware = getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        });
-        return defaultMiddleware.concat(stocksApi.middleware);
-    },
-    devTools: process.env.NODE_ENV !== "production",
+  reducer: {
+    auth: persistedReducer,
+    stock:stockReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
 });
 export let persistor = persistStore(store);
 
