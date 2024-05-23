b5ed5d85691445061f2dc5ef7dfd0910d8aa9589
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:50:29 +0200

    Update Charts.jsx

--- stockApp/src/components/Dashboard/Charts.jsx
+++ stockApp/src/components/Dashboard/Charts.jsx
@@ -1,58 +1,107 @@
-import { AreaChart, Card, Title } from "@tremor/react";
-import Loading from "../Commons/Loading";
-import { useGetPurchasesQuery, useGetSalesQuery } from "../../services/stocks";
 import { Grid } from "@mui/material";
+import { AreaChart } from "@tremor/react";
+import { useSelector } from "react-redux";
+
+const chartdata = [
+  {
+    date: "Jan 22",
+    SemiAnalysis: 2890,
+    "The Pragmatic Engineer": 2338,
+  },
+  {
+    date: "Feb 22",
+    SemiAnalysis: 2756,
+    "The Pragmatic Engineer": 2103,
+  },
+  {
+    date: "Mar 22",
+    SemiAnalysis: 3322,
+    "The Pragmatic Engineer": 2194,
+  },
+  {
+    date: "Apr 22",
+    SemiAnalysis: 3470,
+    "The Pragmatic Engineer": 2108,
+  },
+  {
+    date: "May 22",
+    SemiAnalysis: 3475,
+    "The Pragmatic Engineer": 1812,
+  },
+  {
+    date: "Jun 22",
+    SemiAnalysis: 3129,
+    "The Pragmatic Engineer": 1726,
+  },
+  {
+    date: "Jul 22",
+    SemiAnalysis: 3490,
+    "The Pragmatic Engineer": 1982,
+  },
+  {
+    date: "Aug 22",
+    SemiAnalysis: 2903,
+    "The Pragmatic Engineer": 2012,
+  },
+  {
+    date: "Sep 22",
+    SemiAnalysis: 2643,
+    "The Pragmatic Engineer": 2342,
+  },
+  {
+    date: "Oct 22",
+    SemiAnalysis: 2837,
+    "The Pragmatic Engineer": 2473,
+  },
+  {
+    date: "Nov 22",
+    SemiAnalysis: 2954,
+    "The Pragmatic Engineer": 3848,
+  },
+  {
+    date: "Dec 22",
+    SemiAnalysis: 3239,
+    "The Pragmatic Engineer": 3736,
+  },
+];
 
 const dataFormatter = (number) =>
-  `€${Intl.NumberFormat("de").format(number).toString()}`;
+  `€${Intl.NumberFormat("us").format(number).toString()}`;
 
-const Charts = () => {
-  const { data: purchases } = useGetPurchasesQuery();
-  const { data: sales, isLoading } = useGetSalesQuery();
-
-  const salesData = sales?.map((item) => ({
-    date: new Date(item.createdAt).toLocaleString("de-DE"),
-    sale: Number(item.amount),
+export default function Charts() {
+  const { sales, purchases } = useSelector((state) => state.stock);
+  const salesData = sales.map((item) => ({
+    date: new Date(item.createdAt).toLocaleDateString(),
+    sale: item.amount,
   }));
-
-  const purchasesData = purchases?.map((item) => ({
-    date: new Date(item.createdAt).toLocaleString("de-DE"),
-    purchase: Number(item.amount),
+  const purchasesData = purchases.map((item) => ({
+    date: new Date(item.createdAt).toLocaleString(),
+    purchase: item.amount,
   }));
-  if (isLoading) return <Loading />;
   return (
-    <Grid container justifyContent="center" mt={2} spacing={3}>
+    <Grid container spacing={3} mt={3}>
       <Grid item xs={12} md={6}>
-        <Card>
-          <Title>Sales</Title>
-
-          <AreaChart
-            className="h-72 mt-4"
-            data={salesData}
-            index="date"
-            categories={["sale"]}
-            colors={["lime"]}
-            yAxisWidth={50}
-            valueFormatter={dataFormatter}
-          />
-        </Card>
+        <AreaChart
+          className="h-80"
+          data={salesData}
+          index="date"
+          categories={["sale"]}
+          colors={["indigo", "rose"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={60}
+        />
       </Grid>
-
       <Grid item xs={12} md={6}>
-        <Card>
-          <Title>Purchases</Title>
-          <AreaChart
-            className="h-72 mt-4"
-            data={purchasesData}
-            index="date"
-            categories={["purchase"]}
-            colors={["amber"]}
-            yAxisWidth={50}
-            valueFormatter={dataFormatter}
-          />
-        </Card>
+        <AreaChart
+          className="h-80"
+          data={purchasesData}
+          index="date"
+          categories={["purchase"]}
+          colors={["fuchsia"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={60}
+        />
       </Grid>
     </Grid>
   );
-};
-export default Charts;
+}
