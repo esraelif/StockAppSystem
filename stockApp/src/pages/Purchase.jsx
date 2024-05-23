7adf8739bbe04fdc3c3f4978f9c8dc1bc5883188
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:36:05 +0200

    Update Purchase.jsx

--- stockApp/src/pages/Purchase.jsx
+++ stockApp/src/pages/Purchase.jsx
@@ -8,45 +8,45 @@ import PurchaseTable from "../components/Tables/PurchaseTable";
 import useStockCall from "../hooks/useStockCall";
 
 const Purchases = () => {
-    const { getProPurcFirBrands } = useStockCall();
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => {
-        setOpen(false);
-        setInitialState({
-            brandId: "",
-            firmId: "",
-            productId: "",
-            quantity: "",
-            price: "",
-        });
-    };
-    const [initialState, setInitialState] = useState({
-        brandId: "",
-        firmId: "",
-        productId: "",
-        quantity: "",
-        price: "",
+  const { getProPurcFirBrands } = useStockCall();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      brandId: "",
+      firmId: "",
+      productId: "",
+      quantity: "",
+      price: "",
     });
-    useEffect(() => {
-        getProPurcFirBrands();
-    }, []);
+  };
+  const [initialState, setInitialState] = useState({
+    brandId: "",
+    firmId: "",
+    productId: "",
+    quantity: "",
+    price: "",
+  });
+  useEffect(() => {
+    getProPurcFirBrands();
+  }, []);
 
-    return (
-        <Container maxWidth={"xl"}>
-            <PageHeader text="Purchases" />
-            <MyButton variant="contained" onClick={handleOpen} title="New Purchase" />
-            {open && (
-                <StockModal open={open} handleClose={handleClose}>
-                    <PurchaseForm handleClose={handleClose} initialState={initialState} />
-                </StockModal>
-            )}
-            <PurchaseTable
-                setInitialState={setInitialState}
-                handleOpen={handleOpen}
-            />
-        </Container>
-    );
+  return (
+    <Container maxWidth={"xl"}>
+      <PageHeader text="Purchases" />
+      <MyButton variant="contained" onClick={handleOpen} title="New Purchase" />
+      {open && (
+        <StockModal open={open} handleClose={handleClose}>
+          <PurchaseForm handleClose={handleClose} initialState={initialState} />
+        </StockModal>
+      )}
+      <PurchaseTable
+        setInitialState={setInitialState}
+        handleOpen={handleOpen}
+      />
+    </Container>
+  );
 };
 
 export default Purchases;
