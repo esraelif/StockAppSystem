2209fc308a3a6e6f2c273086606b69a68e13eddf
Esra Elif <150926926+esraelif@users.noreply.github.com>
2024-05-23 20:37:08 +0200

    Update Sales.jsx

--- stockApp/src/pages/Sales.jsx
+++ stockApp/src/pages/Sales.jsx
@@ -8,40 +8,40 @@ import SaleTable from "../components/Tables/SaleTable";
 import useStockCall from "../hooks/useStockCall";
 
 const Sales = () => {
-    const { getProSalBrands } = useStockCall();
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => {
-        setOpen(false);
-        setInitialState({
-            brandId: "",
-            productId: "",
-            quantity: "",
-            price: "",
-        });
-    };
-    const [initialState, setInitialState] = useState({
-        brandId: "",
-        productId: "",
-        quantity: "",
-        price: "",
+  const { getProSalBrands } = useStockCall();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      brandId: "",
+      productId: "",
+      quantity: "",
+      price: "",
     });
-    useEffect(() => {
-        getProSalBrands();
-    }, []);
+  };
+  const [initialState, setInitialState] = useState({
+    brandId: "",
+    productId: "",
+    quantity: "",
+    price: "",
+  });
+  useEffect(() => {
+    getProSalBrands();
+  }, []);
 
-    return (
-        <Container maxWidth={"xl"}>
-            <PageHeader text="Sales" />
-            <MyButton variant="contained" onClick={handleOpen} title="New Sale" />
-            {open && (
-                <StockModal open={open} handleClose={handleClose}>
-                    <SaleForm handleClose={handleClose} initialState={initialState} />
-                </StockModal>
-            )}
-            <SaleTable setInitialState={setInitialState} handleOpen={handleOpen} />
-        </Container>
-    );
+  return (
+    <Container maxWidth={"xl"}>
+      <PageHeader text="Sales" />
+      <MyButton variant="contained" onClick={handleOpen} title="New Sale" />
+      {open && (
+        <StockModal open={open} handleClose={handleClose}>
+          <SaleForm handleClose={handleClose} initialState={initialState} />
+        </StockModal>
+      )}
+      <SaleTable setInitialState={setInitialState} handleOpen={handleOpen} />
+    </Container>
+  );
 };
 
 export default Sales;
